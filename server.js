19ab29ceb1da3bcefd205760f5444f83f113e73b
Roqb Hosler <rph731@gmail.com>
2018-03-01 14:46:11 -0500

    Create draft added

--- server.js
+++ server.js
@@ -23,11 +23,13 @@ var io = socketio.listen(server);
 router.use(express.static(path.resolve(__dirname, 'client')));
 var messages = [];
 var sockets = [];
+var drafts = [];
 
 io.on('connection', function (socket) {
     messages.forEach(function (data) {
       socket.emit('message', data);
     });
+    socket.emit('drafts', drafts);
 
     sockets.push(socket);
 
@@ -35,6 +37,48 @@ io.on('connection', function (socket) {
       sockets.splice(sockets.indexOf(socket), 1);
       updateRoster();
     });
+    
+    socket.on('createDraft', function(playerName, draftType, cube) {
+      //TODO: prevent players from creating multiple drafts
+      var draft = {};
+      draft.id = (Math.random()+1).toString(36).slice(2, 18);
+      draft.playerCount = 1;
+      draft.players = [];
+      draft.players.push(String(playerName || 'Anonymous'));
+      draft.draftType = draftType.name;
+      draft.cube = cube;
+      draft.creationTime = Date.now();
+      draft.displayTime = displayTime();
+      drafts.push(draft);
+      socket.emit('drafts', drafts);
+    });
+    
+    function displayTime() {
+      var str = "";
+  
+      var currentTime = new Date()
+      var hours = currentTime.getHours()
+      var minutes = currentTime.getMinutes()
+      var seconds = currentTime.getSeconds()
+  
+      if (minutes < 10) {
+          minutes = "0" + minutes
+      }
+      if (seconds < 10) {
+          seconds = "0" + seconds
+      }
+      var amPm = "";
+      if(hours > 11){
+          amPm += "PM"
+      } else {
+          amPm += "AM"
+      }
+      if (hours > 12) {
+        hours = hours - 12;
+      }
+      str += hours + ":" + minutes + ":" + seconds + " " + amPm;
+      return str;
+    }
 
     socket.on('message', function (msg) {
       var text = String(msg || '');
